4ecd2c5f06af4ae9b3dcdc252138662df048e550
hosewq <hosewq@gmail.com>
2021-10-11 15:14:15 +0900

    cadvisor installation script

---
+++ install_cadvisor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --volume=/dev/disk/:/dev/disk:ro \
+  --publish=8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  --privileged \
+  --device=/dev/kmsg \
+  gcr.io/cadvisor/cadvisor:v0.41.0 \
+  -storage_driver=influxdb \
+  -storage_driver_host=141.223.181.149:8086 \
+  -storage_driver_db=ni
